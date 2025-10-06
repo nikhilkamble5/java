f295d4f927882384b1da0c860a6f711c9d08c8cf
nikhilkamble5 <nikhilkamble19985@gmail.com>
2025-10-06 17:12:43 +0530

    boolean prograam adn type casting

---
+++ src/Session3.java
@@ -0,0 +1,50 @@
+////boolean
+//public class Session3 {
+//
+//    public static void main(String[] args) {
+//        boolean b= true;
+//        System.out.println(b);
+//
+//        b=false;
+//        System.out.println(b);
+//
+//        b=10>3;
+//        System.out.println(b);
+//
+//        b=10<3;
+//        System.out.println(b);
+//
+//        if(10>3)
+//            System.out.println("Y");
+//        else
+//            System.out.println("N");
+//
+//    }
+//}
+
+
+
+//Character
+
+public  class Session3
+{
+    public static void main(String[] args) {
+
+        int  i=(int)10.23; //double
+        System.out.println(i);
+
+        float f=(float) 10.23;
+        System.out.println(f);
+
+         f= 10.23f;
+        System.out.println(f);
+
+
+        f= 10.23F;
+        System.out.println(f);
+        char ch='A';
+        System.out.println(ch);
+        System.out.println((int)ch);
+
+    }
+}
